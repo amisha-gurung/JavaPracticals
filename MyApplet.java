63a956e28baee3f1d88e1935ac4816118e58e14e
Amsa <amishagurung38@gmail.com>
2025-11-01 20:09:57 +0545

    a simple Java Applet that displays your name and college.

---
+++ MyApplet.java
@@ -0,0 +1,9 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class MyApplet extends Applet {
+    public void paint(Graphics g) {
+        g.drawString("Name: Amisha Gurung", 50, 50);
+        g.drawString("College: PNC College", 50, 80);
+    }
+}
