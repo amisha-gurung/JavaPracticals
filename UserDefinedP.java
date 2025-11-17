794623586a1412fb870f91bcfff2cd40624486fe
Amsa <amishagurung38@gmail.com>
2025-11-17 21:54:53 +0545

    A demo of user defined package in Java

---
+++ UserDefinedP.java
@@ -0,0 +1,8 @@
+import mypack.*;
+public class UserDefinedP {
+    public static void main(String[] args){
+        System.out.println("A demo of user defined package in java");
+        Message message = new Message();
+        message.greetings();
+    }
+}
